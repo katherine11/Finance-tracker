23dbca6213a07e46624126ac0f7d358102be2c69
Katerina Simeonova <Katerina Simeonova>
2016-10-08 12:10:29 +0300

    Apply the extensibility changes in Expense and Income Controllers

--- MyProject/src/main/java/com/example/controller/ObligationController.java
+++ MyProject/src/main/java/com/example/controller/ObligationController.java
@@ -1,40 +1,30 @@
 package com.example.controller;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.model.Obligation;
+import com.example.model.Payment;
 import com.example.model.User;
 import com.example.model.UserHasObligationsDAO;
 import com.example.model.exceptions.PaymentExpeption;
 
-@Controller 
 @ContextConfiguration(classes = UserHasObligationsDAO.class)
-@Scope("session")
-public class ObligationController {
-	
-	@Autowired
-	private UserHasObligationsDAO userHasObligationsDAO;
-	
-	@RequestMapping(value="obligations",method = RequestMethod.POST)
-	public String addObligation(@ModelAttribute Obligation obligation, Model model, HttpServletRequest request){
+public class ObligationController extends PaymentController{
+
+	@Override
+	@RequestMapping(value="/obligations", method = RequestMethod.POST)
+	public String addPayment(Payment payment, Model model, HttpServletRequest request) {
+		if(request.getSession(false) == null){
+			return "index";
+		}
 		
 		User user = (User) request.getSession().getAttribute("user");
 		
-		if (request.getSession(false) == null || user == null || obligation == null){
-			return "index";
-		}
-
 		try {
-			model.addAttribute("obligation", userHasObligationsDAO.insertPayment(user.getUserId(), obligation));
-			userHasObligationsDAO.selectAndAddAllPaymentsOfUser(user);
+			addPayments(payment, model, user);
 		} catch (PaymentExpeption e) {
 			e.printStackTrace();
 			return "error";
@@ -42,6 +32,13 @@ public class ObligationController {
 		
 		return "redirect:/obligations";
 	}
+
+	@Override
+	public String deletePayment(HttpServletRequest request) {
+		return "";
+	}
+	
+
 	
 	
 	
